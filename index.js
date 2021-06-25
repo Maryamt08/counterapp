dc4569f887b3ded348fdbf96a0fcdd01ecbe573c
Maryam Tahir <maryamtah8@gmail.com>
2021-06-25 18:26:41 -0400

    changed variables

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // document.getElementById("count").innerText = 5
 
-let count = 0
+// let count = 0
 
+// console.log(count)
+
+let myAge = 29
+let humanDogRatio = 7
+let count = myAge * humanDogRatio
 console.log(count)
-

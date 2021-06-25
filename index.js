6a9ae4119fb70de65d10297bdeb74c02fcf9b182
Maryam Tahir <maryamtah8@gmail.com>
2021-06-25 18:45:04 -0400

    button added styling started

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // document.getElementById("count").innerText = 5
 
-// let count = 0
+let bonusPoints = 50
+console.log(bonusPoints)
 
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+// let myAge = 29
+// let humanDogRatio = 7
+// let count = myAge * humanDogRatio
 // console.log(count)
 
-let myAge = 29
-let humanDogRatio = 7
-let count = myAge * humanDogRatio
-console.log(count)
+

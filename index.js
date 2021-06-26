1db45b5310c1c7c73cd121868bb0d5d6a1cc93ce
Maryam Tahir <maryamtah8@gmail.com>
2021-06-25 22:49:30 -0400

    save btn added

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-// document.getElementById("count").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
 
-let bonusPoints = 50
-console.log(bonusPoints)
+console.log(saveEl)
 
-bonusPoints = bonusPoints + 50
-console.log(bonusPoints)
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
 
-bonusPoints = bonusPoints - 75
-console.log(bonusPoints)
+function save() {
+    let countStr = count + " - "
 
-bonusPoints = bonusPoints + 45
-console.log(bonusPoints)
+    saveEl.innerText += countStr
 
-// let myAge = 29
-// let humanDogRatio = 7
-// let count = myAge * humanDogRatio
-// console.log(count)
-
-
+    console.log(count)
+}
